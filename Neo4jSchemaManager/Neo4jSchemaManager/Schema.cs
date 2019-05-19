07a8f5b4442232d31e2208d7206ffd4db1a97103
Mike French <michael.french@calibresys.com>
2019-05-19 18:08:24 -0400

    Create Schema.cs and stub Schema.Initialize(*)
    Create Schema.cs and stubs for Schema.Initialize(*). Ensure Driver is optionally accepted.

---
+++ Neo4jSchemaManager/Neo4jSchemaManager/Schema.cs
@@ -0,0 +1,23 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Neo4jSchemaManager
+{
+    public static class Schema
+    {
+        public static void Initialize(Assembly assembly, IDriver driver = null)
+        {
+        }
+
+
+        public static void Initialize(IEnumerable<Type> domainTypes, IDriver driver = null)
+        {
+        }
+
+        public static void Initialize(Type type, IDriver driver = null)
+        {
+        }
+    }
+}
