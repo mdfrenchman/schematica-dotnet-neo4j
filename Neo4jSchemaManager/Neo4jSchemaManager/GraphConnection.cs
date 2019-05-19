3bd5e1d7e1995859c0b844da3a0a8e7087b773ed
Mike French <michael.french@calibresys.com>
2019-05-19 19:09:26 -0400

    Create GraphConnection.cs
    Handle Static Reference to a Neo4j.Driver.

---
+++ Neo4jSchemaManager/Neo4jSchemaManager/GraphConnection.cs
@@ -0,0 +1,32 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neo4jSchemaManager
+{
+    public static class GraphConnection
+    {
+        private static IDriver _neo4jDriver;
+
+        internal static IDriver Driver
+        {
+            get { return _neo4jDriver; }
+        }
+        public static void SetDriver(IDriver driver)
+        {
+            _neo4jDriver = driver;
+        }
+        public static void SetDriver(string boltConnection, IAuthToken authToken)
+        {
+
+            _neo4jDriver = GraphDatabase.Driver(boltConnection, authToken, _driverConfig);
+        }
+        public static void SetDriver(string boltConnection, string username, string password)
+        {
+            _neo4jDriver = GraphDatabase.Driver(boltConnection, AuthTokens.Basic(username, password), _driverConfig);
+        }
+
+        private static Config _driverConfig = new Config { ConnectionTimeout = TimeSpan.FromMilliseconds(-1), MaxTransactionRetryTime = TimeSpan.FromSeconds(60) };
+    }
+}
