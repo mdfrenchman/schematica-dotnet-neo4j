49e6c905559e95db3fc3ba305074f5d4e3a36cf8
Mike French <michael.french@calibresys.com>
2019-05-20 12:00:49 -0400

    NodeExtension Label method
    Label extension plus passing tests for:
    1. Class with Node Attribute and Label property.
    2. Class with no Node Attribute.

---
+++ Neo4jSchemaManager/Neo4jSchemaManager.Tests/Extensions/NodeExtension_Tests.cs
@@ -0,0 +1,30 @@
+﻿
+using Xunit;
+using Neo4jSchemaManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neo4jSchemaManager.Tests.Extensions
+{
+    public class NodeExtension_Tests
+    {
+        #region Label
+        [Fact]
+        public void NodeExtension_Label_With_NodeAttribute_Returns_AttributeProperty_Label()
+        {
+            var testType = typeof(DomainTestSample.Vehicle);
+            var actual = testType.Label();
+            Xunit.Assert.NotEqual("Vehicle", actual);
+            Xunit.Assert.Equal("Car", actual);
+        }
+
+        [Fact]
+        public void NodeExtension_Label_With_OUT_NodeAttribute_Returns_ClassName()
+        {
+            var testType = typeof(DomainTestSample.Person);
+            Xunit.Assert.Equal("Person", testType.Label());
+        }
+        #endregion
+    }
+}
---
+++ Neo4jSchemaManager/Neo4jSchemaManager/Extensions/NodeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Neo4j.Schema.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Neo4jSchemaManager
+{
+    public static class NodeExtensions
+    {
+        public static string Label(this Type type)
+        {
+            // Check Label value in the Node Attribute.
+            NodeAttribute attr = (NodeAttribute)type.GetCustomAttribute(typeof(NodeAttribute), false);
+            if (!(attr is null) && !String.IsNullOrEmpty(attr.Label))
+                return attr.Label;
+            // Get the ClassName if there is no Node Attribute with a Label defined.
+            string friendlyName = type.Name;
+            if (type.IsGenericType)
+            {
+                int iBacktick = friendlyName.IndexOf('`');
+                if (iBacktick > 0)
+                {
+                    friendlyName = friendlyName.Remove(iBacktick);
+                }
+                friendlyName += $"<{string.Join(",", type.GetGenericArguments().Select(p => type.Label()))}>";
+            }
+
+            return friendlyName;
+        }
+    }
+}
