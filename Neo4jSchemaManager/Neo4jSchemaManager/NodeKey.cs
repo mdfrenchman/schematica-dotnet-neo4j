9f3b4ff77763d6deb24ef711978150624a01ed36
Mike French <michael.french@calibresys.com>
2019-05-20 10:50:24 -0400

    NodeKey transaction methods
    stubs to CRUDE (E is for Exists) NodeKey on the graph.

---
+++ Neo4jSchemaManager/Neo4jSchemaManager/NodeKey.cs
@@ -0,0 +1,44 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neo4jSchemaManager
+{
+    internal static class NodeKey
+    {
+        internal static void SetNodeKeyConstraint(this Type type, ITransaction tx)
+        {
+        }
+
+        internal static void RemoveNodeKeyConstraint(this Type type, ITransaction tx)
+        {
+
+        }
+
+        private static bool MatchesExisting(this Type type, ITransaction tx)
+        {
+            return false;
+        }
+
+        private static bool Exists(this Type type, ITransaction tx)
+        {
+            return String.IsNullOrEmpty(type.Get(tx));
+        }
+
+        private static void Create(this Type type, ITransaction tx)
+        {
+
+        }
+
+        private static void Drop(this Type type, ITransaction tx)
+        {
+
+        }
+
+        private static string Get(this Type type, ITransaction tx)
+        {
+            return String.Empty;
+        }
+    }
+}
