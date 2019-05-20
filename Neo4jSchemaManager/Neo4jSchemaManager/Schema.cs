d23a2e0f1ae90ae47d05122c0ebe43a96f745eb4
Mike French <michael.french@calibresys.com>
2019-05-20 10:51:55 -0400

    Schema.Initialize method bodies

--- Neo4jSchemaManager/Neo4jSchemaManager/Schema.cs
+++ Neo4jSchemaManager/Neo4jSchemaManager/Schema.cs
@@ -9,15 +9,33 @@ namespace Neo4jSchemaManager
     {
         public static void Initialize(Assembly assembly, IDriver driver = null)
         {
+            Initialize(assembly.ExportedTypes, driver);
         }
 
 
         public static void Initialize(IEnumerable<Type> domainTypes, IDriver driver = null)
         {
+            if (driver is null)
+                driver = GraphConnection.Driver;
+            using (var session = driver.Session(AccessMode.Write))
+            {
+                session.WriteTransaction(tx => {
+                    foreach (Type type in domainTypes)
+                    {
+                        type.SetNodeKeyConstraint(tx);
+                    }
+                });
+            }
         }
 
         public static void Initialize(Type type, IDriver driver = null)
         {
+            if (driver is null)
+                driver = GraphConnection.Driver;
+            using (var session = driver.Session(AccessMode.Write))
+            {
+                session.WriteTransaction(tx => type.SetNodeKeyConstraint(tx));
+            }
         }
     }
 }
