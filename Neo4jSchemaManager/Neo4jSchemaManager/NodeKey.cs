bc0d3e5ab4624f70138a55f851ec2a6ae45d6cd4
Mike French <michael.french@calibresys.com>
2019-05-22 19:15:27 -0400

    add logic for SetNodeKeyConstraint

--- Neo4jSchemaManager/Neo4jSchemaManager/NodeKey.cs
+++ Neo4jSchemaManager/Neo4jSchemaManager/NodeKey.cs
@@ -1,6 +1,7 @@
 ﻿using Neo4j.Driver.V1;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Neo4jSchemaManager
@@ -9,6 +10,11 @@ namespace Neo4jSchemaManager
     {
         internal static void SetNodeKeyConstraint(this Type type, ITransaction tx)
         {
+            if (!type.MatchesExisting(tx))
+            {
+                type.Drop(tx);
+                type.Create(tx);
+            }
         }
 
         internal static void RemoveNodeKeyConstraint(this Type type, ITransaction tx)
@@ -18,7 +24,11 @@ namespace Neo4jSchemaManager
 
         private static bool MatchesExisting(this Type type, ITransaction tx)
         {
-            return false;
+            var existing = type.Get(tx);
+            if (String.IsNullOrEmpty(existing))
+                return false;
+            else 
+                return (existing == type.NodeKeyConstraintString());
         }
 
         private static bool Exists(this Type type, ITransaction tx)
@@ -28,17 +38,47 @@ namespace Neo4jSchemaManager
 
         private static void Create(this Type type, ITransaction tx)
         {
-
+            if (!type.Exists(tx))
+            {
+                var constraint = type.NodeKeyConstraintString();
+                if (!String.IsNullOrEmpty(constraint))
+                    tx.Run($"CREATE {constraint}");
+            }
         }
 
         private static void Drop(this Type type, ITransaction tx)
         {
+            // TODO: For calls from SetConstraint, we're calling Exists twice. Need to refactor.
+            if (type.Exists(tx))
+            {
+                var constraint = type.NodeKeyConstraintString();
+                if (!String.IsNullOrEmpty(constraint))
+                    tx.Run($"DROP {constraint}");
+            }
 
         }
 
         private static string Get(this Type type, ITransaction tx)
         {
-            return String.Empty;
+            var record = tx.Run("call db.constraints() yield description WITH * WHERE description contains $typeName AND description contains 'NODE KEY' RETURN description", new { typeName = type.Label() }).FirstOrDefault();
+            if (record is null)
+                return String.Empty;
+            else if (!record[0].As<string>().Contains(") ASSERT ("))
+                return record[0].As<string>().Replace(") ASSERT ", ") ASSERT ( ").Replace(" IS NODE KEY", " ) IS NODE KEY");
+            else
+                return record[0].As<string>();
+        }
+
+        private static string NodeKeyConstraintString(this Type type)
+        {
+            var nodeKeyParams = type.NodeKey();
+            var label = type.Label();
+            var nodeVariable = label.ToLower();
+            var keyString = String.Join(", ", type.NodeKey().Select(nk => $"{nodeVariable}.{nk}"));
+            if (String.IsNullOrEmpty(keyString))
+                return String.Empty;
+            else
+                return $"CONSTRAINT ON ( {nodeVariable}:{label} ) ASSERT ({keyString}) IS NODE KEY";
         }
     }
 }
