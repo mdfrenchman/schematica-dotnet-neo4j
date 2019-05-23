7511c80cfaf83d9127a77dc440d68d2671d0153b
Mike French <michael.french@calibresys.com>
2019-05-22 22:03:37 -0400

    Create Schema_Initialize_Tests.cs

---
+++ Neo4jSchemaManager/Neo4jSchemaManager.Tests/Schema_Initialize_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Neo4jSchemaManager.Tests
+{
+    public class Schema_Initialize_Tests
+    {
+
+        [Fact]
+        public void Initialize_Will_SetNodeKey_For_Type()
+        {
+
+        }
+
+        [Fact]
+        public void Initialize_Will_SetNodeKeys_For_CollectionOf_Types()
+        {
+
+        }
+
+        [Fact]
+        public void Initialize_Will_SetNodeKeys_For_All_Types_In_Assembly()
+        {
+
+        }
+
+        [Fact]
+        public void Initialize_Will_SetIndexes_For_Type()
+        {
+
+        }
+
+        [Fact]
+        public void Initialize_Will_SetIndexes_For_CollectionOf_Types()
+        {
+
+        }
+
+        [Fact]
+        public void Initialize_Will_SetIndexes_For_All_Types_In_Assembly()
+        {
+
+        }
+
+    }
+}
