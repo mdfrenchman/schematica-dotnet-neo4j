5bdd7120f60cba996e6517a7cb66545cdf3876a1
Mike French <michael.french@calibresys.com>
2019-05-23 21:09:49 -0400

    Add Index instance Create, Drop, Exists

--- Neo4j.Schema/Neo4j.Schema/Indexes/Index.cs
+++ Neo4j.Schema/Neo4j.Schema/Indexes/Index.cs
@@ -7,13 +7,41 @@ namespace Neo4j.Schema.Indexes
 {
     public class Index
     {
+
+        // new Schema.Indexes.Index() => Schema.Indexes.Index { Label, Properties };
+        // (Schema.Indexes.Index instance).Create(tx|session|driver);
+        // (Schema.Indexes.Index instance).Drop(tx|session|driver);
+        // (Schema.Indexes.Index instance).Exists(tx|session|driver);
+
+
+        private string Label { get; set; }
+
+        private string[] Properties { get; set; }
+
+        public Index(){ }
+        public Index(string label, string[] properties)
+        {
+            Label = label;
+            Properties = properties;
+        }
+
         public void Create(IDriver driver = null) { }
 
         public void Create(ISession session) { }
 
         public void Create(ITransaction tx) { }
 
-        // new Schema.Indexes.Index() => Schema.Indexes.Index { Label, Properties };
-        // (Schema.Indexes.Index instance).Create(tx|session|driver);
+        public void Drop(IDriver driver = null) { }
+
+        public void Drop(ISession session) { }
+
+        public void Drop(ITransaction tx) { }
+
+        public bool Exists(IDriver driver = null) { return false; }
+
+        public bool Exists(ISession session) { return false; }
+
+        public bool Exists(ITransaction tx) { return false; }
+
     }
 }
