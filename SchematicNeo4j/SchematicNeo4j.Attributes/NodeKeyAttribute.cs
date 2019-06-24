79c576bb50f69d0ec75fb6678900bcfdcb879701
Mike French <michael.french@calibresys.com>
2019-06-24 12:57:12 -0400

    Add documentation to NodeKeyAttribute

--- SchematicNeo4j/SchematicNeo4j.Attributes/NodeKeyAttribute.cs
+++ SchematicNeo4j/SchematicNeo4j.Attributes/NodeKeyAttribute.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace SchematicNeo4j.Attributes
 {
+    /// <summary>
+    /// Assign a property to a Nodes NodeKey Constraint.
+    /// </summary>
+    /// <remarks>
+    /// Need to do some further testing around using Inherited to true. 
+    /// Makes sense with a lot of cases, but not sure we've thought of all of them.
+    /// </remarks>
     [System.AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
     public class NodeKeyAttribute : Attribute { }
 
