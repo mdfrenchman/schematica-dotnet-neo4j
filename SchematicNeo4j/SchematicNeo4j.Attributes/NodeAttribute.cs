adfbc8eceaf2921ac8048084ffac0b9bab9d1308
Mike French <michael.french@calibresys.com>
2019-06-24 12:56:52 -0400

    Add documentation to NodeAttribute

--- SchematicNeo4j/SchematicNeo4j.Attributes/NodeAttribute.cs
+++ SchematicNeo4j/SchematicNeo4j.Attributes/NodeAttribute.cs
@@ -4,6 +4,15 @@ using System.Text;
 
 namespace SchematicNeo4j.Attributes
 {
+    /// <summary>
+    /// Indicates a Class is a Graph Node.
+    /// </summary>
+    /// <remarks>
+    /// The class name will be used for the Label unless a Label property is set.
+    /// </remarks>
+    /// <example>
+    /// [Node(Label="Vehicle:Car")] | [Node(Label="Person")] | [Node]
+    /// </example>
     [System.AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
     public class NodeAttribute : Attribute
     {
