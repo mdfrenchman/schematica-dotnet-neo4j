a2649a846497c8ec714294897385bc6404b0c8b7
Mike French <michael.french@calibresys.com>
2019-11-04 15:25:42 -0500

    Create IndexAttribute.cs

---
+++ SchematicNeo4j/SchematicNeo4j.Attributes/IndexAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchematicNeo4j.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = true)]
+    public class IndexAttribute : Attribute 
+    {
+        public string Name;
+    }
+}
