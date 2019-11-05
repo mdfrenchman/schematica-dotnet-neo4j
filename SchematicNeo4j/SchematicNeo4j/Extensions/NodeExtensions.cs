76ce920b3b98edfb7063997f8da1403e51a2edda
Mike French <michael.french@calibresys.com>
2019-11-05 15:11:46 -0500

    Add Indexes NodeExtension

--- SchematicNeo4j/SchematicNeo4j/Extensions/NodeExtensions.cs
+++ SchematicNeo4j/SchematicNeo4j/Extensions/NodeExtensions.cs
@@ -33,5 +33,22 @@ namespace SchematicNeo4j
                 type.Name;
 
         }
+
+        public static List<Index> Indexes(this Type type)
+        {
+            var nodeType = type;
+            PropertyInfo[] propertyInfo = nodeType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var result = propertyInfo.SelectMany(pi => 
+                ((IndexAttribute[])pi.GetCustomAttributes(typeof(IndexAttribute), true))
+                    .Select(ia => (name: ((IndexAttribute)ia).Name, label: ((IndexAttribute)ia).Label, prop: pi.Name))
+                ).GroupBy(t => (t.name, t.label))
+                .Select(grouping => new Index(
+                        name: grouping.Key.name,
+                        label: String.IsNullOrWhiteSpace(grouping.Key.label) ? type.Label() : grouping.Key.label,
+                        properties: grouping.Select(t => t.prop).ToArray()
+                        )
+                ).ToList();
+            return result;
+        }
     }
 }
