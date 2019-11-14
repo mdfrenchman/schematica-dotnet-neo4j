8a8c94f59614aad0f9bc367f443a8e7a2b461e31
Mike French <michael.french@calibresys.com>
2019-11-14 16:25:43 -0500

    Add IndexDropTests

--- SchematicNeo4j/SchematicNeo4j.Tests/Indexes/Index_Drop_Tests.cs
+++ SchematicNeo4j/SchematicNeo4j.Tests/Indexes/Index_Drop_Tests.cs
@@ -9,100 +9,96 @@ using SchematicNeo4j;
 
 namespace SchematicNeo4j.Tests.Indexes
 {
-    public class Index_Create_Tests : IDisposable
+    public class Index_Drop_Tests : IDisposable
     {
         private IDriver driver = null;
-        private readonly Index testIndex = new Index(label: "IndexCreateTest", new string[] { "Prop1", "Prop2" });
-        public Index_Create_Tests()
+        private readonly Index testIndex = new Index(label: "IndexDropTest", new string[] { "Prop1", "Prop2" });
+        public Index_Drop_Tests()
         {
             driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "scratch"));
         }
 
         [Fact]
-        public void Index_Create_Will_CreateIndex_By_Label_And_Properties_With_Driver()
+        public void Index_Drop_Will_DropIndex_By_Label_And_Properties_With_Driver()
         {
-            // Ensure Index does not exist.
-            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
-            // Execute
-            testIndex.Create(driver);
-            // Test Outcome
+            // Ensure Index does exist.
+            CreateIndexForTest();
             Assert.Equal(testIndex, GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
+            // Execute
+            testIndex.Drop(driver);
+            // Test Outcome
+            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
         }
 
         [Fact]
-        public void Index_Create_Will_CreateIndex_By_Label_And_Properties_With_GraphConnection_SetDriver()
+        public void Index_Drop_Will_DropIndex_By_Label_And_Properties_With_GraphConnection_SetDriver()
         {
             GraphConnection.SetDriver(driver);
-            // Ensure Index does not exist.
-            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
-            // Execute
-            testIndex.Create();
-            // Test Outcome
+            // Ensure Index does exist.
+            CreateIndexForTest();
             Assert.Equal(testIndex, GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
+            // Execute
+            testIndex.Drop();
+            // Test Outcome
+            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
         }
 
         [Fact]
-        public void Index_Create_Will_ThrowException_With_No_Driver()
+        public void Index_Drop_Will_ThrowException_With_No_Driver()
         {
-            // Ensure Index does not exist.
-            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
+            // Ensure Index does exist.
+            CreateIndexForTest();
+            Assert.Equal(testIndex, GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
             // Ensure the GraphConnection.Driver are null.
             GraphConnection.SetDriver(null);
             // Execute
-            Assert.Throws<Neo4jException>(() => testIndex.Create(driver: null));
+            Assert.Throws<Neo4jException>(() => testIndex.Drop(driver: null));
         }
 
         [Fact]
-        public void Index_Create_Will_CreateIndex_By_Label_And_Properties_With_Session()
+        public void Index_Drop_Will_DropIndex_By_Label_And_Properties_With_Session()
         {
-            // Ensure Index does not exist.
-            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
+            // Ensure Index does exist.
+            CreateIndexForTest();
+            Assert.Equal(testIndex, GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
             // Execute
             using (ISession session = driver.Session(AccessMode.Write))
             {
-                testIndex.Create(session);
+                testIndex.Drop(session);
             }
             // Test Outcome
-            Assert.Equal(testIndex, GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
-        }
-
-
-
-        [Fact]
-        public void Index_Create_Will_CreateIndex_By_Label_And_Properties_But_Name_Is_UnnamedIndex()
-        {
-            // Ensure Index does not exist.
             Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
-            // Execute
-            testIndex.Create(driver);
-            // Test Outcome
+        }
+
+
+
+        [Fact]
+        public void Index_Drop_Will_DropIndex_By_Label_And_Properties_But_Name_Is_UnnamedIndex()
+        {
+            // Ensure Index does exist.
+            CreateIndexForTest();
             var actual = GetIndexForTest_KeepUnnamedIndex(testIndex);
             Assert.Equal(testIndex.Label, actual.Label);
             Assert.Equal(testIndex.Properties, actual.Properties);
             Assert.NotEqual(testIndex.Name, actual.Name);
-            Assert.Equal("Unnamed index", actual.Name);
+            Assert.Equal("Unnamed index", actual.Name); 
+            // Execute
+            testIndex.Drop(driver);
+            // Test Outcome
+            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(testIndex));
         }
 
         [Fact]
-        public void Index_Create_Will_Succeeed_When_Index_Already_Exists_By_Label_And_Properties()
+        public void Index_Drop_Will_Do_Nothing_When_Index_Doesnt_Exist_By_Label_And_Properties()
         {
-            // Ensure Index exists on the graph.
-            CreateIndexForTest();
-            var preExisting = GetIndexForTest_KeepUnnamedIndex(testIndex);
-            Assert.Equal(testIndex.Label, preExisting.Label);
-            Assert.Equal(testIndex.Properties, preExisting.Properties);
-            Assert.NotEqual(testIndex.Name, preExisting.Name);
-            Assert.Equal("Unnamed index", preExisting.Name);
-
+            var nonExistantIndex = new Index("noExist", "SomethingThatDoesntExistForATest", new string[] { "Sally", "Bob" });
+            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(nonExistantIndex));
             // Execute
-            testIndex.Create(driver);
+            nonExistantIndex.Drop(driver);
 
             // Test Outcome
-            var actual = GetIndexForTest_KeepUnnamedIndex(testIndex);
-            Assert.Equal(testIndex.Label, actual.Label);
-            Assert.Equal(testIndex.Properties, actual.Properties);
-            Assert.NotEqual(testIndex.Name, actual.Name);
-            Assert.Equal("Unnamed index", actual.Name);
+            Assert.Null(GetIndexForTest_OverloadUnnamedIndexAsNull(nonExistantIndex));
+
 
         }
 
@@ -112,7 +108,7 @@ namespace SchematicNeo4j.Tests.Indexes
             {
                 session.WriteTransaction(tx => {
                     if (testIndex.Exists(tx))
-                        tx.Run("DROP INDEX ON :IndexCreateTest(Prop1,Prop2)");
+                        tx.Run("DROP INDEX ON :IndexDropTest(Prop1,Prop2)");
                 });
             }
         }
@@ -122,7 +118,7 @@ namespace SchematicNeo4j.Tests.Indexes
         {
             using (ISession session = driver.Session(AccessMode.Write))
             {
-                session.WriteTransaction(tx => tx.Run("CREATE INDEX ON :IndexCreateTest(Prop1,Prop2)"));
+                session.WriteTransaction(tx => tx.Run("CREATE INDEX ON :IndexDropTest(Prop1,Prop2)"));
             }
         }
 
